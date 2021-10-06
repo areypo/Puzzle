19776f3f25533d18429f1493cdf62511876718d8
areypo <areypo@uoc.edu>
2021-10-06 19:12:44 +0200

    Splash Screen added

---
+++ app/src/main/java/com/manytiles/p8/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.manytiles.p8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //  Splash
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+            startActivity(intent);
+        },4000);
+    }
+}
